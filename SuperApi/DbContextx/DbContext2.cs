452ab2b805c2659758f370fd48439aec3a9475fd
kgalaszewski <37513352+kgalaszewski@users.noreply.github.com>
2020-06-08 20:51:22 +0200

    yes

--- SuperApi/DbContextx/DbContext2.cs
+++ SuperApi/DbContextx/DbContext2.cs
@@ -9,7 +9,8 @@ namespace SuperApi.DbContextx
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseSqlServer("Server=tcp:superapi20200603113044dbserver.database.windows.net,1433;Initial Catalog=SuperApi20200603113044_db;Persist Security Info=False;User ID=adminusernamexD;Password=admin123@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            var connectionString = "";
+            builder.UseSqlServer(connectionString);
         }
     }
 }
